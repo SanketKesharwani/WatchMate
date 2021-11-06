35273019edf74c2bdeb3371143f8efb4ee04238c
Sanket-Code-98 <98sanketkesharwani@gmail.com>
2021-11-06 12:12:20 +0530

    movie_list and movie_detail views

--- watchlist/api/views.py
+++ watchlist/api/views.py
@@ -1,4 +1,17 @@
 from watchlist.models import Movie
+from rest_framework.response import Response
+from watchlist.api.serializers import MovieSerializer
+from rest_framework.decorators import api_view
 
+@api_view()
 def movie_list(request):
-    pass
+    movies = Movie.objects.all()
+    serializer = MovieSerializer(movies,many=True)
+    print(serializer.data)
+    return Response(serializer.data)
+
+@api_view()
+def movie_details(request,pk):
+    movie = Movie.objects.get(pk=pk)
+    serializer = MovieSerializer(movie)
+    return Response(serializer.data)
