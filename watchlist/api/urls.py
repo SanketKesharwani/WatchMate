4524658baa5f9a16ab89999008a386f9e118636a
Sanket-Code-98 <98sanketkesharwani@gmail.com>
2021-11-09 13:14:36 +0530

    stream and watchlist url mapping

--- watchlist/api/urls.py
+++ watchlist/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path,include
 # from watchlist.api.views import movie_list,movie_details
-from watchlist.api.views import MovieListAV,MovieDetailAV
+from watchlist.api.views import WatchListAV,WatchDetailAV,StreamPlatformAV,StreamPlatformDetailAV
 
 urlpatterns =[
-    path('list/',MovieListAV.as_view(),name='movie-list'),
-    path('<int:pk>/',MovieDetailAV.as_view(),name='movie-details')
+    path('list/',WatchListAV.as_view(),name='movie-list'),
+    path('<int:pk>/',WatchDetailAV.as_view(),name='movie-details'),
+    path('stream/',StreamPlatformAV.as_view(),name='stream-platform'),
+    path('stream/<int:pk>/',StreamPlatformDetailAV.as_view(),name='stream-platform'),
 ]
