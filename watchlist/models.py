dbd4c331e4ae3a64ca6bfbfaaebb05113315394f
Sanket-Code-98 <98sanketkesharwani@gmail.com>
2021-11-12 11:25:50 +0530

    created review model

--- watchlist/models.py
+++ watchlist/models.py
@@ -1,16 +1,7 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+# Create your models here. 
 
-# Create your models here.
-
-class WatchList(models.Model):
-    title = models.CharField(max_length=50)
-    storyline = models.CharField(max_length=200)
-    activate = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-       return self.title
-   
 class StreamPlatform(models.Model):
     name = models.CharField(max_length=100)
     about = models.CharField(max_length=200)
@@ -18,3 +9,24 @@ class StreamPlatform(models.Model):
     
     def __str__(self):
        return self.name
+
+class WatchList(models.Model):
+    title = models.CharField(max_length=50)
+    storyline = models.CharField(max_length=200)
+    platform = models.ForeignKey(StreamPlatform, on_delete=models.CASCADE, related_name="watchlist")
+    activate = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+       return self.title
+   
+class Review(models.Model):
+    rating = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator((5))])
+    description = models.CharField(max_length=200,null=True, blank=True)
+    watchlist = models.ForeignKey(WatchList, on_delete=models.CASCADE, related_name="reviews")
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return str(self.rating)+" | "+self.watchlist.title
