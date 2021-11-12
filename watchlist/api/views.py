589245284ddfca9a8925daada69d541452105062
Sanket-Code-98 <98sanketkesharwani@gmail.com>
2021-11-12 11:26:38 +0530

    reviewdetail and review list views

--- watchlist/api/views.py
+++ watchlist/api/views.py
@@ -1,13 +1,35 @@
-from watchlist.models import WatchList,StreamPlatform
+from watchlist.models import WatchList,StreamPlatform,Review
 from rest_framework.response import Response
 from rest_framework import status
 #from rest_framework.decorators import api_view
 from rest_framework.views import APIView
-from watchlist.api.serializers import WatchListSerializer,StreamPlatformSerializer
-
+from watchlist.api.serializers import WatchListSerializer,StreamPlatformSerializer,ReviewSerializer
+from rest_framework import mixins
+from rest_framework import generics
+    
 #----------------------------------------------------------------------------------------
 #   class based api views
 #-----------------------------------------------------------------------------------------
+
+class ReviewDetail(mixins.RetrieveModelMixin, generics.GenericAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+class ReviewList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
 class WatchListAV(APIView):
     def get(self, request):
         movies = WatchList.objects.all()
