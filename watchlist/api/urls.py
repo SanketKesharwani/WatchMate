47c54c275bbf3a4cae66987c44c3e3973fee78b3
Sanket-Code-98 <98sanketkesharwani@gmail.com>
2021-11-13 12:48:12 +0530

    structured url for reviews

--- watchlist/api/urls.py
+++ watchlist/api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path,include
 # from watchlist.api.views import movie_list,movie_details
-from watchlist.api.views import WatchListAV,WatchDetailAV,StreamPlatformAV,StreamPlatformDetailAV,ReviewList,ReviewDetail
+from watchlist.api.views import WatchListAV,WatchDetailAV,StreamPlatformAV,StreamPlatformDetailAV,ReviewList,ReviewDetail,ReviewCreate
 
 urlpatterns =[
     path('list/',WatchListAV.as_view(),name='movie-list'),
     path('<int:pk>/',WatchDetailAV.as_view(),name='movie-details'),
     path('stream/',StreamPlatformAV.as_view(),name='stream-platform'),
     path('stream/<int:pk>/',StreamPlatformDetailAV.as_view(),name='stream-platform'),
-    path('review/',ReviewList.as_view(),name='review-list'),
-    path('review/<int:pk>/',ReviewDetail.as_view(),name='review-detail'),
+    path('stream/<int:pk>/review/',ReviewList.as_view(),name='review-list'),
+    path('stream/<int:pk>/review-create/',ReviewCreate.as_view(),name='review-create'),
+    path('stream/review/<int:pk>/',ReviewDetail.as_view(),name='review-detail'),
 ]
