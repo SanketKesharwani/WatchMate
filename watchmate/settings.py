f736a314d021a98c971a532390263163e933428e
Sanket-Code-98 <98sanketkesharwani@gmail.com>
2021-11-23 12:01:08 +0530

    throttle setting

--- watchmate/settings.py
+++ watchmate/settings.py
@@ -140,7 +140,15 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ]
+    ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '1/day',
+        'user': '2/day'
+    }
 
 }
 
