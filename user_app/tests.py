35a03b8981a3e5b5b625dc69e793f1c245c242cb
Sanket-Code-98 <98sanketkesharwani@gmail.com>
2021-11-26 12:22:19 +0530

    automated testing

--- user_app/tests.py
+++ user_app/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
 
-# Create your tests here.
+class RegisterTestCase(APITestCase):
+    def test_register(self):
+        data = {
+            'username': 'testcase',
+            'email': 'testcase@xyz.com',
+            'password':'testpass',
+            'password2':'testcase'
+        }
+        response  = self.client.post(reverse('register'),data)
+        self.assertEqual(response.status_code,status.HTTP_201_CREATED)
+        
+class LoginLogoutTestcase(APITestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create(username='testuser',password='password123')
+        
+    def test_login(self):
+        data = {
+            "username":"testuser",
+            "password":"password123"
+        }
+        response  = self.client.post(reverse('login'),data)
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        
+    def test_logout(self):
+        self.token = Token.objects.get(user__username="testuser")
+        self.client.credentials(HTTP_AUTHORIZATION='Token '+self.token.key)
+        response  = self.client.post(reverse('logout'))
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        
