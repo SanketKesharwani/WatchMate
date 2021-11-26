d9920e757d834cb3f6e15bb580f89748cc88f054
Sanket-Code-98 <98sanketkesharwani@gmail.com>
2021-11-26 12:22:34 +0530

    status code change

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -29,5 +29,5 @@ def registration_view(request):
         else:
             data = serializer.errors
             
-        return Response(data)
+        return Response(data,status=status.HTTP_201_CREATED)
     
